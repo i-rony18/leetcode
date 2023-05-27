5b6a840ec5ce928de6409b727606bc3e2d4d6651
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2023-05-27 18:08:31 +0000

    Sync LeetCode submission - Number of Laser Beams in a Bank (cpp)

---
+++ my-folder/problems/number_of_laser_beams_in_a_bank/solution.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>&bank) {
+        int count;
+        vector<int>laser;
+        for (int i=0; i<size(bank); i++){
+            count=0;
+            for (int j=0; j<size(bank[0]); j++){
+                if (bank[i][j]=='1'){
+                    count+=1;
+                }
+            }
+        if (count>0){
+            laser.push_back(count);
+        }
+        
+        }
+        if (size(laser)<=1){
+            return 0;
+        }
+
+        long long sum=0;
+        for (int i=0; i<size(laser)-1; i++){
+            sum+=laser[i]*laser[i+1];
+        }
+        return sum;
+
+    }
+};
