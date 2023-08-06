5d8b55b0647dc0ddcfc3c41bad10057000671b88
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2023-08-06 17:47:54 +0000

    Sync LeetCode submission - Implement Queue using Stacks (cpp)

---
+++ my-folder/problems/implement_queue_using_stacks/solution.cpp
@@ -0,0 +1,39 @@
+class MyQueue {
+    stack<int> st;
+    void getStack(stack<int> &s, int x){
+        if(s.empty()){
+            s.push(x);
+            return;
+        }
+        int temp=s.top();
+        s.pop();
+        getStack(s,x);
+        s.push(temp);
+    }
+public:
+    void push(int x) {
+        getStack(st,x);
+    }
+    
+    int pop() {
+        int ans=st.top();
+        st.pop();
+        return ans;
+    }
+    
+    int peek() {
+        return st.top();
+    }
+    
+    bool empty() {
+        return st.empty();
+    }
+};
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
