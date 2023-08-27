dbf2443adb0e67e2ae42af811aaa8f1578205bee
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2023-08-27 18:57:47 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- my-folder/problems/valid_anagram/solution.cpp
+++ my-folder/problems/valid_anagram/solution.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
-       return s==t;
+        vector<int> freq(26,0);
+        if(s.size()!= t.size()){
+            return false;
+        }
+        for(int i=0; i<s.size(); i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+       
     }
 };
