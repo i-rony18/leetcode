93f9ba06b528856f8fdd733f4f7720256a4a69dd
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2023-12-19 19:29:28 +0000

    Sync LeetCode submission - Image Smoother (cpp)

---
+++ my-folder/problems/image_smoother/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size(), n = img[0].size();
+        vector<vector<int>> result(m, vector<int>(n));
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int sum = 0, count = 0;
+                for (int dr = -1; dr <= 1; dr++){
+                    for (int dc = -1; dc <= 1; dc++) {
+                        int nr = r + dr;
+                        int nc = c + dc;
+                        if ((nr < 0) || (nr >= m)) continue;
+                        if ((nc < 0) || (nc >= n)) continue;
+                        sum += img[nr][nc];
+                        count++;
+                    }
+                }
+                result[r][c] = (sum / count);
+            }
+        }
+        return result;
+    }
+};
