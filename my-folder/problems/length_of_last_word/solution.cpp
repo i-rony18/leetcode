ec8b3ced505009e486c92b90702e5beaae2a1892
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2024-04-01 17:03:08 +0000

    Sync LeetCode submission - Length of Last Word (cpp)

--- my-folder/problems/length_of_last_word/solution.cpp
+++ my-folder/problems/length_of_last_word/solution.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i,j,k,c=0;
-        i=s.size()-1;
-        while(i>=0 and s[i]==' ')
-        i--;
-        for(;i>=0;i--){
-            if(s[i]==' ' and c!=0)
-            return c;
-            c++;
+        int len=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' '){
+                if(len>0)return len;
+            }
+            else len++;
         }
-        return c;
-        
+        return len; 
     }
 };
