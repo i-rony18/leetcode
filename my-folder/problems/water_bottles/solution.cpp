d270dfd01a9b63bbec62ba969ef22870d654bc8a
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2024-07-07 07:05:59 +0000

    Sync LeetCode submission - Water Bottles (cpp)

--- my-folder/problems/water_bottles/solution.cpp
+++ my-folder/problems/water_bottles/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        
-    int ans=numBottles;
-    int empty = numBottles;
-    while(empty >= numExchange)
-    {
-        ans += empty/numExchange ;
-        empty = empty % numExchange + empty/numExchange; 
-    }
-    return ans;
-
+        int ans=numBottles;
+        int remain=0;
+        while(numBottles/numExchange != 0){
+            ans+=numBottles/numExchange;
+            numBottles= numBottles/numExchange + numBottles%numExchange;
+            
+        }
+        return ans;
     }
 };
