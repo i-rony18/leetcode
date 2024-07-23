7d696cd7d6523b5c7a834df854c61e3208a41a40
Ronit Das <59742444+i-rony18@users.noreply.github.com>
2024-07-23 15:25:23 +0000

    Sync LeetCode submission - Sort Array by Increasing Frequency (cpp)

---
+++ my-folder/problems/sort_array_by_increasing_frequency/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto it: nums){
+            mp[it]++;
+        }
+        sort(nums.begin(), nums.end(), [&](int n1, int n2) {
+            if (mp[n1] != mp[n2]) {
+                return mp[n1] < mp[n2];
+            } 
+            else {
+                return n2 < n1;
+            }
+    });
+    return nums;
+    }
+    
+};
